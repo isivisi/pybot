fbb46f164298c54c354425ebba5ca847a6e5a7c9
John Iannandrea <jiannandrea@gmail.com>
2016-02-28 17:41:57 -0500

    Added --config

--- pybot.py
+++ pybot.py
@@ -32,12 +32,23 @@ def main():
                 pyLoc +" pybot.py %*")
                 file.close()
                 print("Created windows bat file")
-                sys.path.append(os.getcwd()+"pybot.bat")
+                sys.path.append(os.getcwd()+"\\pybot.bat")
 
             #TODO linux
             print("[Setup] pybot added to system PATH")
 
             print("pybot setup complete")
+
+        elif sys.argv[1] == "--config":
+            if len(sys.argv) > 2:
+                settings = data.Settings().getConf()
+                args = sys.argv[2].split(".")
+                settings.set(args[0], args[1], sys.argv[3])
+
+                with open('pybot.conf', 'wb') as configfile:
+                    settings.write(configfile)
+            else:
+                print("Invalid use, type pybot -help")
 
         elif sys.argv[1] == "-help":
             help()
@@ -48,7 +59,11 @@ def help():
         print("Pybot Help\n" \
         "\t-run\tRuns pybot\n" \
         "\t-setup\tCreates config file\n" \
-        "\t-help\tI really wonder...\n")
+        "\t-help\tI really wonder...\n\n" \
+        "\t--config\n" \
+        "\t\tbot.name\n" \
+        "\t\tbot.auth\n" \
+        "\t\ttwitch.channel\n\t\tetc...")
 
 if __name__ == "__main__":
     main()
