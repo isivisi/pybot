70b9147ac861f05705ca64fa91d60f4232bf5558
John Iannandrea <jiannandrea@gmail.com>
2016-02-28 05:19:46 -0500

    addex regex to website filter

--- filters/websites.py
+++ filters/websites.py
@@ -1,16 +1,15 @@
 import sys
 import random
+import re
 
 name = sys.argv[0]
 msg = sys.argv[1]
 
-filters 		= ['http://', 'www.', '.com', '.ca', '.org', '.gov', '.on', '.tk']
+#@diegoperini website regex
+REGEX = r"_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS"
 KICK_MSGS 		= ["please dont post links without permission.", "please ask before posting a link."]
 
-for filter in filters:
-	if filter in msg.lower() and "permit" not in self.getMode(name):
-		
-		pybotPrint("[FILTER][WEBSITES.PY] " + name, "filter")
-		self.kick(name)
-		self.msg(name + " " + KICK_MSGS[random.randint(0, len(KICK_MSGS)-1)])
-		break
+if re.match(REGEX, msg) != "":
+	pybotPrint("[FILTER][WEBSITES.PY] " + name, "filter")
+	self.kick(name)
+	self.msg(name + " " + KICK_MSGS[random.randint(0, len(KICK_MSGS)-1)])
