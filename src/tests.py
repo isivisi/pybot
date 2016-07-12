9e8a50b90a0ad1c0e9a54c30a00ca2fb6c10da1f
John Iannandrea <johniannandrea@gmail.com>
2016-07-11 20:06:39 -0400

    Added setup check to tests (#33)

--- src/tests.py
+++ src/tests.py
@@ -9,11 +9,23 @@ if ("\\pybot\\src" in os.getcwd()):
 else:
     src = os.path.join(os.getcwd(), "src")
 
+currentTest = 0
+def startTest():
+    global currentTest
+    currentTest += 1
+
 def main():
-    # Compile code to make sure its syntactically correct
-    print("[PYBOT TESTS]\n")
-    print("[TEST 1] Simple syntax check")
-    compileTest()
+    global currentTest
+    try:
+        # Compile code to make sure its syntactically correct
+        print("[PYBOT TESTS]\n")
+        print("[TEST 1] SIMPLE SYNTAX TEST")
+        compileTest()
+        print("[TEST 2] SETUP CHECK")
+        setupTest()
+    except AssertionError as e:
+        print("[TEST %s] ASSERTION FAILED: %s" % (currentTest, str(e),))
+        exit(1)
 
 def getPyFilesFromDir(dir = ""):
     loc = os.path.join(src, dir)
@@ -21,6 +33,7 @@ def getPyFilesFromDir(dir = ""):
     return [os.path.join(dir, f) for f in os.listdir(loc) if os.path.isfile(os.path.join(loc, f)) and ".py" in f and not ".pyc" in f and not "py." in f] # todo regex
 
 def compileTest():
+    startTest()
     print("Filding files...")
     subfolders = [s[0] for s in os.walk(src)]
     files = []
@@ -31,6 +44,22 @@ def compileTest():
     for file in files:
         py_compile.compile(file, doraise=True)
         print(file + " compiled with no errors")
+
+def setupTest():
+    startTest()
+    print("Checking if setup was performed correctly...")
+
+    print("importing tornado...")
+    import tornado
+    print("importing requests...")
+    import requests
+
+    print("looking for chart.min.js...")
+    assert (os.path.isfile(os.path.join(src, "web", "Chart.min.js")) == True), "Chart.min.js not found!"
+    print("looking for normalize.css...")
+    assert (os.path.isfile(os.path.join(src, "web", "css",  "normalize.css")) == True), "normalize.css not found!"
+    print("looking for skeleton.css...")
+    assert (os.path.isfile(os.path.join(src, "web", "css", "skeleton.css")) == True), "skeleton.css not found!"
 
 if __name__ == "__main__":
     main()
